db559acb70f9b9d83fb3bdfe93e560e6ef17f5f8
Lluis Danes <lluisdanes@gmail.com>
2022-09-23 16:01:44 +0200

    use dynamic routing to add spot in url

--- pages/[[...slug]].js
+++ pages/[[...slug]].js
@@ -1,4 +1,4 @@
-import React, { useState, useDebugValue  } from 'react'
+import React, { useState, useDebugValue, useEffect } from 'react'
 import { getSortedSpotsData } from '../lib/spots'
 import Layout from '../components/Layout/Layout'
 import Map from '../components/Map'
@@ -12,21 +12,24 @@ import { useRouter } from 'next/router'
 function MapPage({ allSpotsData, blurImages }) {
   const router = useRouter()
   const slug = router.query.slug || []
-
-  if (slug.length === 1) {
-    const pumptrackIdFromQuery = slug[0]
-    const requestedSpot = allSpotsData.spots.find(spot => spot.id === pumptrackIdFromQuery)
-    if (requestedSpot) {
-      handleSelectedPumptrack(requestedSpot)
-    }
-  }
-
   const center = [41.979401, 2.821426]
   const mapZoom = 10
   const bounds = [[42.27917879724292, 2.254910573201578],[41.62981728626594, 3.2354221997777097]]
   const [pumptrack, setPumptrack] = useState(null)
   const [markerSelected, setMarkerSelected] = useState(false)
+
+  useEffect(() => {
+    if (slug.length === 1) {
+      const pumptrackIdFromQuery = slug[0]
+      const requestedSpot = allSpotsData.spots.find(spot => spot.id === pumptrackIdFromQuery)
+      if (requestedSpot) {
+        handleSelectedPumptrack(requestedSpot)
+      }
+    }
+  }, [])
+
   function handleSelectedPumptrack (selectedPumptrack) {
+    router.push(`/${selectedPumptrack.id}`, `/${selectedPumptrack.id}`, { shallow: true })
     setMarkerSelected(true)
     setPumptrack(selectedPumptrack)
   }
@@ -49,17 +52,29 @@ function MapPage({ allSpotsData, blurImages }) {
   )
 }
 
-export async function getStaticProps() {
-  const allSpotsData = await getSortedSpotsData()
-  const blurImages = await getImagePlaceholders(allSpotsData)
+export async function getStaticProps(params) {
+  const isHomePage = !!!params.slug
 
-  return {
-    props: {
-      allSpotsData,
-      blurImages
+  if (isHomePage) {
+    const allSpotsData = await getSortedSpotsData()
+    const blurImages = await getImagePlaceholders(allSpotsData)
+
+    return {
+      props: {
+        allSpotsData,
+        blurImages
+      }
     }
   }
 }
 
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { slug: false }},
+    ],
+    fallback: 'blocking',
+  }
+}
 
 export default MapPage
