7d08e756a3a368112f8af08423f40ecfcbbcdf1a
Lluis Danes <lluisdanes@gmail.com>
2023-04-19 10:35:13 +0200

    add gtag script

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import createEmotionCache from "../utils/createEmotionCache"
 import { CacheProvider } from "@emotion/react"
 import CookieConsent from "../components/CookieConsent/CookieConsent"
 import { appWithTranslation } from 'next-i18next'
+import Script from 'next/script'
 
 const clientSideEmotionCache = createEmotionCache()
 
@@ -15,6 +16,16 @@ function MyApp({
   return (
       <CacheProvider value={emotionCache}>
         <ThemeProvider theme={theme}>
+          <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-C7YNG3T7HH" />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-C7YNG3T7HH');
+            `}
+          </Script>
           <Component {...pageProps} />
           <CookieConsent/>
         </ThemeProvider>
